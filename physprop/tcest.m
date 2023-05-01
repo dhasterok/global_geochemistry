c7d003b41bbb777d885175a509154d5d1282d34a
dhasterok <48115114+dhasterok@users.noreply.github.com>
2023-05-01 11:57:58 +0930

    updated physprop files to work with updated oxide_norm

--- physprop/tcest.m
+++ physprop/tcest.m
@@ -14,9 +14,15 @@ function data = tcest(data)
 
 %data.thermal_conductivity = exp( (1.72*tmp.sio2 + 1.018*tmp.mgo ...
 %    - 3.652*tmp.na2o - 1.791*tmp.k2o)/100 );
-data.thermal_conductivity = exp(4.899*data.sio2 + 0.01736*data.tio2 + ...
-    0.8895*data.al2o3 + 6.641*data.feo_tot + 2.136*data.mgo + 0.7416*data.cao + ...
-    0.05848*data.na2o + 0.2804*data.k2o);
+
+% results from Willcocks et al. (in prep)
+oxides = {'SiO2'; 'TiO2'; 'Al2O3'; 'FeO'; 'MgO'; 'CaO'; 'Na2O'; 'K2O'};
+oxlist = {'sio2'; 'tio2'; 'al2o3'; 'feo_tot'; 'mgo'; 'cao'; 'na2o'; 'k2o'};
+tmp = oxide_norm(data,'Normalization','anhydrous','Oxides',oxlist);
+
+data.thermal_conductivity = exp((1.5542*tmp.sio2 - 6.5211*tmp.tio2 ...
+    - 0.2547*tmp.al2o3 + 2.0660*tmp.feo_tot + 0.5472*tmp.mgo ...
+    + 0.2258*tmp.cao - 2.4017*tmp.na2o - 0.6721*tmp.k2o)./sum(tmp{:,oxlist},2));
 
 ind = (data.thermal_conductivity < 1.5 | 5 < data.thermal_conductivity);
 data.thermal_conductivity(ind) = nan;
